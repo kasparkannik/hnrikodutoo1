7ea6c0fc6a181efc51c09437596594b408ebc9f9
kasparkannik <kaspar.kannik@outlook.com>
2022-09-08 23:25:28 +0300

    edits n stuff

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 
 const app = express();
-
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function(request, response, next){
     
